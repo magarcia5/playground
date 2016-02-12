d898a4716600105bb7a4e50033ea531a36d77dc8
Mel Alrajhi <mgarcia@splunk.com>
2016-02-12 12:56:16 -0800

    adding home state

--- my_blog_app/static/app.js
+++ my_blog_app/static/app.js
@@ -3,7 +3,8 @@ var app = angular.module('flapperNews', ['ui.router']);
 app.factory('posts', [function(){
 	var o = {
 		posts: [
-			{title: "This is hilarious!", link: "https://www.youtube.com/watch?v=SNYOEgMeSvM", upvotes: 7}
+			{title: "This is hilarious!", link: "https://www.youtube.com/watch?v=SNYOEgMeSvM", upvotes: 7},
+			{title: "Superbowl halftime show", link: "https://www.youtube.com/watch?v=L_Hgh7sPDLM", upvotes: 11}
 		]
 	}
 	return o;
