3cfed5b8157162c2ff58539db7a8b32171675a3f
Mel Alrajhi <mgarcia@splunk.com>
2016-02-16 14:54:28 -0800

    preloading post

--- my_blog_app/flapper-news/routes/index.js
+++ my_blog_app/flapper-news/routes/index.js
@@ -27,5 +27,22 @@ router.post('/posts', function(req, res, next){
 	post.save(function(err, post){
 		if(err){ return next(err); }
 		res.json(post);
-	})
-})
+	});
+});
+
+route.param('post', function(req, res, next, id){
+	var query = Post.findById(id);
+
+	query.exec(function(err, post){
+		if(err) { return next(err); }
+		if(!post){ return next(new Error('can\'t find post')); }
+
+		// returns id
+		req.post = post;
+		return next();
+	});
+});
+
+router.get('/posts/:post', function(req, res){
+	res.json(req.post);
+});
