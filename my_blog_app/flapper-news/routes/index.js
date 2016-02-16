c8f681df4008e718a75a972cf78585bc568cf7e1
Mel Alrajhi <mgarcia@splunk.com>
2016-02-16 14:37:31 -0800

    adding get and post endpoints for posts

--- my_blog_app/flapper-news/routes/index.js
+++ my_blog_app/flapper-news/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+// retrieve all posts
+router.get('/posts', function(req, res, next){
+	// query all posts, next() is err handling function
+	Post.find(function(err, posts){
+		if(err){ return next(err); }
+		res.json(posts);
+	});
+});
+
+// add one post
+router.post('/posts', function(req, res, next){
+	var post = new Post(req.body);
+	post.save(function(err, post){
+		if(err){ return next(err); }
+		res.json(post);
+	})
+})
