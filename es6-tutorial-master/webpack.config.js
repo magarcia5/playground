196e15b9a0fdc4c33922366b423b140b6ab2893a
Mel Alrajhi <mgarcia@splunk.com>
2017-04-04 21:02:48 -0700

    switching to webpack

---
+++ es6-tutorial-master/webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './js/main.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'main.bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/, // target file extension?
+                loader: 'babel-loader', // plugin we installed with npm
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map' // mapping compiled file to original for debug in browser
+}
